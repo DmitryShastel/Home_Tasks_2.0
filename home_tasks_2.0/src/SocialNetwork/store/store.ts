e85bb016f5614ffdc624dcbf561733db3d82d32e
unknown <dmshastel@gmail.com>
2024-01-08 15:04:14 +0300

    The SocialNetwork: store is done

--- home_tasks_2.0/src/SocialNetwork/store/store.ts
+++ home_tasks_2.0/src/SocialNetwork/store/store.ts
@@ -1,1 +1,9 @@
-export default {}
+import {combineReducers, createStore} from "redux";
+import {profileReducer} from "./profileReducer";
+
+const rootReducer = combineReducers({
+    posts: profileReducer,
+})
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+export const store = createStore(rootReducer)
