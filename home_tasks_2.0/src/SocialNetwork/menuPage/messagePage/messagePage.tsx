5595b4ad945f5a9dc81e600b2b81cea91506c858
unknown <dmshastel@gmail.com>
2024-01-08 12:34:22 +0300

    The SocialNetwork: MessagePage test is done

--- home_tasks_2.0/src/SocialNetwork/menuPage/messagePage/messagePage.tsx
+++ home_tasks_2.0/src/SocialNetwork/menuPage/messagePage/messagePage.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import messagePageStyle from './messagePage.module.css'
 
 export const MessagePage = () => {
-    return (
+
+    const messageState = {
+        dialog: [
+            {id: 1, name: 'Dima'},
+            {id: 2, name: 'Alex'},
+        ],
+        message: [
+            {id: 1, message: 'hi'},
+            {id: 2, message: 'by'},
+        ],
+        newMessageText: ''
+    }
+
+    const dialogsList = messageState.dialog.map(d =>
         <div>
-            MessagePage
+            <span><img/></span>
+            <span>{d.name}</span>
+        </div>
+    )
+
+    const messageList = messageState.message.map(m =>
+        <div>
+            <span><img/></span>
+            <span>{m.message}</span>
+        </div>
+    )
+
+
+    return (
+        <div className={messagePageStyle.container}>
+            <div className={messagePageStyle.block}>{dialogsList}</div>
+            <div className={messagePageStyle.block}>
+                {messageList}
+                <div>
+                    <input/>
+                    <div>
+                        <button>Add message</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
